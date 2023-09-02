4770d493f086ea06c2b32566cdda5ef079771493
rajeshpillai <pillai.rajesh@gmail.com>
2023-09-02 15:57:21 +0530

    Nav update text

--- app/components/ui/navbar/index.tsx
+++ app/components/ui/navbar/index.tsx
@@ -36,7 +36,7 @@ export default function NavBar() {
         </div>
       </div>
       <div className="navbar-center">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className="btn btn-ghost normal-case text-xl">[RAJESH]</a>
       </div>
       <div className="navbar-end">
         <button className="btn btn-ghost btn-circle">
