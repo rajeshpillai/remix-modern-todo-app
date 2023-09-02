eb1be94281ced502c67c7d64fc23603fca749b19
rajeshpillai <pillai.rajesh@gmail.com>
2023-09-02 15:08:53 +0530

    Styling subtasks

--- app/routes/todo.view.tsx
+++ app/routes/todo.view.tsx
@@ -66,18 +66,18 @@ export default function TodosList() {
                     <td></td>
                     <td colSpan={4}>
                       {todo.subtasks.length > 0 ? (
-                        <table className="min-w-full bg-white divide-y divide-gray-200">
-                          <thead className="bg-gray-50">
+                        <table className="min-w-full  divide-y divide-gray-200 bg-gray-100">
+                          <thead className="bg-blue-100 text-gray-700">
                             <tr>
                               <th className="w-6/12 py-2 text-center text-gray-500">Subtask Title</th>
                               <th className="w-6/12 py-2 text-center text-gray-500">Status</th>
                             </tr>
                           </thead>
-                          <tbody className="bg-white divide-y divide-gray-200">
+                          <tbody className="bg-red-100 divide-y divide-gray-200">
                             {todo.subtasks.map((subtask: any) => (
                               <tr key={subtask.id}>
-                                <td className="py-2">{subtask.title}</td>
-                                <td className="py-2">{subtask.status}</td>
+                                <td className="py-2 px-2">{subtask.title}</td>
+                                <td className="py-2 px-2">{subtask.status}</td>
                               </tr>
                             ))}
                           </tbody>
